adfd626312001927135b197476d65585ccb3045d
jkrueger721 <johnathankrueger@me.com>
2018-05-08 13:52:35 -0400

    changes

--- ngApp/controllers/createController.js
+++ ngApp/controllers/createController.js
@@ -2,30 +2,34 @@
     constructor(projectService, $state) {
         this.projectService = projectService;
         this.$state = $state;
+       
+       console.log('CreateController');
       }
        
       addProject() {
+        let uID = sessionStorage.getItem('userId');
         let vm = {
+            
             project: this.projectToCreate,
-            userId: 1
+            userId: uID
         };
         console.log("from CreateContorller", vm);
         this.projectService.save(vm).then(
             () => this.$state.go('home')
         );
-    //     var fsClient = filestack.init('Ab6WXYLeSC60vuczv05zQz');
-    //     function upLoad()  {
-            
-    //     fsClient.pick({
-    //         fromSources:["local_file_system","imagesearch","facebook","instagram","dropbox"],
-    //         accept:["audio/*"]
-    //     }).then(function(response) {
-    //         // declare this function to handle response
-    //         handleFilestack(response);
-    //   }); 
-    // }
-
     }
+        upload()  {
+        var fsClient = filestack.init('Ab6WXYLeSC60vuczv05zQz');  
+        console.log('filstack upload');
+        fsClient.pick({
+            fromSources:["local_file_system","imagesearch","facebook","instagram","dropbox"],
+            accept:["audio/*"]
+        }).then(function(response) {
+            // declare this function to handle response
+            handleFilestack(response);
+      }); 
+    }
+
 }
 
 
